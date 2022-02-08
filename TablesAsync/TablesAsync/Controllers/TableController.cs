8b0c725b8c6e6634020ea9cccea472f1b4bd89a1
Adam <69144123+Adam-Howarth@users.noreply.github.com>
2022-02-08 18:28:29 +0000

    Added my project

---
+++ TablesAsync/TablesAsync/Controllers/TableController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TablesAsync.Controllers
+{
+	public class TableController : Controller
+	{
+		public ActionResult Desktop()
+		{
+			return new PartialViewResult
+			{
+				ViewName = "~/Views/Tables/_DesktopTable.cshtml"
+			};
+		}
+
+		public ActionResult Mobile()
+		{
+			return new PartialViewResult
+			{
+				ViewName = "~/Views/Tables/_MobileTable.cshtml"
+			};
+		}
+	}
+}
---
+++ TablesAsync/TablesAsync/Scripts/asyncTables.js
@@ -0,0 +1,31 @@
+﻿var initTables = () => {
+	var isMobile;
+
+	const fetchTable = () => {
+
+		isMobile = window.innerWidth < 768;
+
+		$.ajax({
+			url: isMobile ? '/Table/Mobile' : '/Table/Desktop',
+			contentType: 'application/html; charset=utf-8',
+			type: 'GET',
+			dataType: 'html',
+			success: (result) => {
+				$('#tableRoot').html(result);
+			},
+			error: (xhr, status) => {
+				alert(status);
+			}
+		});
+	};
+
+	$(document).ready(() => {
+		fetchTable();
+	});
+
+	$(window).resize(() => {
+		if (isMobile != (window.innerWidth < 768)) {
+			fetchTable();
+		}
+	});
+}
